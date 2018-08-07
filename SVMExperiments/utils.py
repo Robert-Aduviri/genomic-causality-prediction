05eefd97c4428b3870e0c5caa9e48db290ad1e8d
Robert-Alonso <robert.alonso.ac@gmail.com>
2018-08-06 20:05:42 -0500

    Add parallel processing

--- SVMExperiments/utils.py
+++ SVMExperiments/utils.py
@@ -1,4 +1,6 @@
 import re
+import multiprocessing as mp
+from functools import partial
 import numpy as np
 import pandas as pd
 from scipy.io import loadmat
@@ -48,7 +50,10 @@ def log_metrics(targets, preds, run, feat_ranking, classifier, n_features, descr
           f'TP: {tp} | TN: {tn} | FP: {fp} | FN: {fn}'
     return tpr, tnr, gmean, log
 
-def get_bag_data(data, bags, bag_idx, bag_id, features):
+def bag_idx(n_samples, x):
+    return range(n_samples * (x-1), n_samples * x)
+
+def get_bag_data(data, bags, n_samples, bag_id, features):
     '''
     data: pd.DataFrame
     bags: numpy matrix
@@ -57,55 +62,91 @@ def get_bag_data(data, bags, bag_idx, bag_id, features):
     features: list of ints
     '''
     bag = data.loc[[x for i in bags[:, bag_id] \
-                    for x in bag_idx(i)]]
-    # all columns except metadata (5:) and target (:-1)
-    return bag.iloc[:, 5:-1].iloc[:, features]
+                    for x in bag_idx(n_samples, i)]]
+    
+    return bag.iloc[:, features]
 
-def predict_bags(trn_neg, trn_pos, test_data, test_labels,
-                 run, classifier, classifier_name, param, 
-                 feature_ranker, features, bags, bag_idx, bag_id):
+def predict_bag(bag_id, trn_neg, trn_pos, test_data, test_labels,
+                 run, classifier, classifier_name, params, 
+                 feature_ranker, features, bags, n_samples):
     trn_neg_bag_data = get_bag_data(trn_neg, bags['NegGenePairsBag'], 
-                                    bag_idx, bag_id, features)
+                                    n_samples, bag_id, features)
     trn_pos_bag_data = get_bag_data(trn_pos, bags['PosGenePairsBag'], 
-                                    bag_idx, bag_id, features)
+                                    n_samples, bag_id, features)
 
     train_data = np.concatenate([trn_pos_bag_data, trn_neg_bag_data])
     train_labels = np.concatenate([np.ones(len(trn_pos_bag_data)), 
                                    np.zeros(len(trn_neg_bag_data))])
 
-    model = classifier()
+    model = classifier(**params)
     model.fit(train_data, train_labels)
     test_preds = model.predict(test_data)
-    all_preds.append(test_preds)
+
     tpr, tnr, gmean, log = log_metrics(test_labels, test_preds, 
                                    run, feature_ranker, classifier_name,
                                    len(features), f'Bag: {bag_id}')
-    return tpr, tnr, gmean, log
+
+    return [tpr, tnr, gmean], log, test_preds
+
+import ctypes
 
 def evaluate_bags(trn_neg, trn_pos, test_data, test_labels, 
-                  run, classifier, classifier_name, param, 
-                  feature_ranker, features, bags, bag_idx, num_bags):
-    
-    results = []
-    for bag_id in range(max(num_bags)):
-        results.append(predict_bags(trn_neg, trn_pos, test_data, test_labels,
-                 run, classifier, classifier_name, param, 
-                 feature_ranker, features, bags, bag_idx, bag_id))
+                  run, classifier, classifier_name, params, 
+                  feature_ranker, features, bags, n_samples, num_bags):
+    # all columns except metadata (5:) and target (:-1)    
+    trn_neg = trn_neg.iloc[:, 5:-1]
+    trn_pos = trn_pos.iloc[:, 5:-1]
 
-    for log in all_logs:
+    trn_neg_shape = trn_neg.shape
+    trn_neg = mp.Array('d', trn_neg.values.reshape(-1))
+    trn_neg = np.ctypeslib.as_array(trn_neg.get_obj())
+    trn_neg = trn_neg.reshape(trn_neg_shape)
+    
+    trn_pos_shape = trn_pos.shape
+    trn_pos = mp.Array('d', trn_pos.values.reshape(-1))
+    trn_pos = np.ctypeslib.as_array(trn_pos.get_obj())
+    trn_pos = trn_pos.reshape(trn_pos_shape)
+    
+    test_data_shape = test_data.shape
+    test_data = mp.Array('d', test_data.values.reshape(-1))
+    test_data = np.ctypeslib.as_array(test_data.get_obj())
+    test_data = test_data.reshape(test_data_shape)
+    
+    test_labels_shape = test_labels.shape
+    test_labels = mp.Array('d', test_labels.values.reshape(-1))
+    test_labels = np.ctypeslib.as_array(test_labels.get_obj())
+    test_labels = test_labels.reshape(test_labels_shape)
+    
+
+    partial_predict = partial(predict_bag, trn_neg=trn_neg, trn_pos=trn_pos, 
+                            test_data=test_data, test_labels=test_labels, run=run,
+                            classifier=classifier, classifier_name=classifier_name,
+                            params=params, feature_ranker=feature_ranker, features=features,
+                            bags=bags, n_samples=n_samples)
+
+    pool = mp.Pool()
+    results = pool.map(partial_predict, range(max(num_bags)))
+    pool.close()
+    pool.join()
+
+    # results = []
+    # for bag_id in range(max(num_bags)):
+    #     results.append(partial_predict(bag_id))
+    
+    metrics, logs, preds = zip(*results)
+    for log in logs:
         print(log)
 
-    all_preds = np.array(all_preds)
-    all_metrics = np.array(all_metrics)
+    preds = np.array(preds)
+    metrics = np.array(metrics)
     for n_bags in num_bags:
         # [100, 200, 300]
-        print(' '*12, f'Number of bags: {n_bags}')
-        log = f'Mean TPR: {all_metrics[:n_bags, 0].mean():.4f} | ' \
-              f'Mean TNR: {all_metrics[:n_bags, 1].mean():.4f} | ' \
-              f'Mean GMean: {all_metrics[:n_bags, 2].mean():.4f}'
+        log = f'Mean TPR: {metrics[:n_bags, 0].mean():.4f} | ' \
+              f'Mean TNR: {metrics[:n_bags, 1].mean():.4f} | ' \
+              f'Mean GMean: {metrics[:n_bags, 2].mean():.4f}'
         print(log)
         tpr, tnr, gmean, log = log_metrics(test_labels, 
-                    (all_preds[:n_bags, :].mean(axis=0) > 0.5).astype(int), 
+                    (preds[:n_bags, :].mean(axis=0) > 0.5).astype(int), 
                     run, feature_ranker, classifier_name, 
                     len(features), f'Ensemble of {n_bags} bags')
         print(log)
@@ -122,14 +163,8 @@ def classify_feature_rank(DataCV_dir, Bags_dir, FeatRanking_dir, classifiers, pa
                                 f'({pval_pos_threshold}).trn.mat')
     test = load_mat(DataCV_dir/f'{feature_set}/{dataset}_{treatment}'
                                f'({pval_pos_threshold}).tst.mat')
-    
     feat_rank = pd.read_csv(FeatRanking_dir/f'{feature_set}/{dataset}_{treatment}'
                                             f'({pval_pos_threshold}).csv', sep=';')
-    
-    #if test_all_features:
-        # add all features (6 metadata columns)
-    #    num_top_features += [train.shape[1] - 6]
-    
     trn_neg = train[train.Target == 0].reset_index(drop=True)
     trn_pos = train[train.Target == 1].reset_index(drop=True)
     
@@ -141,45 +176,41 @@ def classify_feature_rank(DataCV_dir, Bags_dir, FeatRanking_dir, classifiers, pa
         n_samples = 6 if treatment == 'All' else 3
         assert bags['NegGenePairsBag'].max() == len(train[train.Target==0]) // n_samples
         assert bags['PosGenePairsBag'].max() == len(train[train.Target==1]) // n_samples
-        bag_idx = lambda x: range(n_samples * (x-1), n_samples * x)
         
         feat_rank_rows = feat_rank[feat_rank.Run == run]
         
         for classifier, param in zip(classifiers, params):
-                # [LGBMClassifier, LinearSVC, RandomForestClassifier]
-                classifier_name = str(classifier).strip("<>'").split('.')[-1]
-                print(' '*6, f'Classifier: {classifier_name}')
+            # [LGBMClassifier, LinearSVC, RandomForestClassifier]
+            classifier_name = str(classifier).strip("<>'").split('.')[-1]
+            print(' '*3, f'Classifier: {classifier_name}')
         
             for idx, feat_rank_row in feat_rank_rows.iterrows():
                 # [Entropy, Ttest, Brattacharyya, Wilcoxon]
-                print(' '*3, f'Feature ranking method: {feat_rank_row.Method}')             
+                print(' '*6, f'Feature ranking method: {feat_rank_row.Method}')             
                 
                 for num_top_feats in num_top_features:
                     # [2, 4, 6, 7, 10, 12] matlab index starts at 1
                     features = [col - 1 for col in feat_rank_row[2:2+num_top_feats]]
-                    print(' '*6, f'Features: {features}')
+                    print(' '*9, f'Features: {features}')
                     
                     test_data = test.iloc[:,5:-1].iloc[:,features]
                     test_labels = test.Target
- 
                     evaluate_bags(trn_neg, trn_pos, test_data, test_labels, 
                                   run, classifier, classifier_name, param, 
-                                  feat_rank_row.Method, features, bags, bag_idx, num_bags)    
-    
+                                  feat_rank_row.Method, features, bags, n_samples, num_bags)    
             if test_all_features:
-                print(' '*3, f'All features')
+                print(' '*6, f'All features')
                 # use all features (6 metadata columns)
-                num_top_feats = [train.shape[1] - 6]
+                num_top_feats = train.shape[1] - 6
                 # [2, 4, 6, 7, 10, 12] matlab index starts at 1
                 features = list(range(num_top_feats))
-                print(' '*6, f'Features: {features}')
+                print(' '*9, f'Features: {features}')
 
                 test_data = test.iloc[:,5:-1].iloc[:,features]
                 test_labels = test.Target
-
                 evaluate_bags(trn_neg, trn_pos, test_data, test_labels, 
                               run, classifier, classifier_name, param, 
-                              feat_rank_row.Method, features, bags, bag_idx, num_bags)
+                              feat_rank_row.Method, features, bags, n_samples, num_bags)
                     
                     
                           
